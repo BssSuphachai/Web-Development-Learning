fcd5e5ad6300ba7d5dc78c9fab2e60ecb096d225
T <suphachai.bss@gmail.com>
2026-07-05 14:24:09 +0700

    Week1

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+const box1 = 10;
+const box2 = 5;
+const box3 = "ศุภชัย";
+let box4 = box1 + box2;
+
+console.log(box3);
+console.log(box4);
+
+function sayhi() {
+  document.getElementById("show-text").innerHTML = "สวัสดี" + " " + box3 + " " + "วันนี้เป็นอย่างไรบ้าง";
+}
